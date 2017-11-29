6fcb09c1c128d72a440b7356b980792c9f520dda
Nidhi Panpalia <npanpalia@cs.stonybrook.edu>
2017-11-29 03:33:55 -0500

    Removed garbage collection function definitions
    Signed-off-by: Nidhi Panpalia <npanpalia@cs.stonybrook.edu>

--- dm-dedup-target.c
+++ dm-dedup-target.c
@@ -24,6 +24,7 @@
 #include "dm-dedup-cbt.h"
 #include "dm-dedup-kvstore.h"
 #include "dm-dedup-check.h"
+#include "dm-dedup-gc.h"
 
 #define MAX_DEV_NAME_LEN (64)
 
@@ -988,81 +989,6 @@ static void dm_dedup_status(struct dm_target *ti, status_type_t status_type,
 		       dc->metadata_dev->name, dc->data_dev->name, dc->block_size,
 			dc->crypto_alg, dc->backend_str, dc->flushrq);
 	}
-}
-
-static int gc_process_cleanup_hash_pbn(void *key, int32_t ksize, void *value,
-			    s32 vsize, void *data)
-{
-	int r = 0;
-	u64 pbn_val = 0;
-	struct hash_pbn_value hashpbn_value = *((struct hash_pbn_value *)value);
-	struct dedup_config *dc = (struct dedup_config *)data;
-
-	BUG_ON(!data);
-
-	pbn_val = hashpbn_value.pbn;
-
-	if (dc->mdops->get_refcount(dc->bmd, pbn_val) == 1) {
-		r = dc->kvs_hash_pbn->kvs_delete(dc->kvs_hash_pbn,
-							key, ksize);
-		if (r < 0)
-			goto out;
-		r = dc->mdops->dec_refcount(dc->bmd, pbn_val);
-		if (r < 0)
-			goto out_dec_refcount;
-
-		dc->physical_block_counter -= 1;
-		dc->gc_blocks_cleaned++;
-	}
-
-	goto out;
-
-out_dec_refcount:
-	dc->kvs_hash_pbn->kvs_insert(dc->kvs_hash_pbn, key,
-			ksize, (void *)&hashpbn_value,
-			sizeof(hashpbn_value));
-out:
-	return r;
-}
-
-static int gc_process(struct dedup_config *dc)
-{
-	int err = 0;
-	
-	/* Set garbage collection status flag to true */
-	dc->gc_status = true;
-
-	BUG_ON(!dc);
-
-	/* Cleanup hashes if the refcount of block == 1 */
-	err = dc->kvs_hash_pbn->kvs_iterate(dc->kvs_hash_pbn,
-			&gc_process_cleanup_hash_pbn, (void *)dc);
-
-	/* Set garbage collection status flag to false */
-	dc->gc_status = false;
-
-	return err;
-}
-
-static int gc_blocks_estimate_count(void *key, int32_t ksize, void *value,
-                            int32_t vsize, void *data)
-{
-        struct dedup_config *dc = (struct dedup_config *)data;
-        dc->gc_blocks_estimate++;
-        return 0;
-}
-
-static int gc_blocks_estimate(struct dedup_config *dc)
-{
-        int err = 0;
-
-        BUG_ON(!dc);
-
-        /* Count if the refcount of block == 1 */
-        err = dc->kvs_hash_pbn->kvs_iterate(dc->kvs_hash_pbn,
-                        &gc_blocks_estimate_count, (void *)dc);
-
-        return err;
 }
 
 static int dm_dedup_message(struct dm_target *ti,
